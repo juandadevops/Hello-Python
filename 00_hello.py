8a1be0974830c96cc0af6fbf038f0519e7797792
juandaaa1 <jcg700@alumnos.unican.es>
2024-02-26 00:24:41 +0100

    Add files day 1 training

---
+++ 00_hello.py
@@ -0,0 +1,12 @@
+# Hello World comment
+print("Hello World")
+
+
+'''
+Consultar el tipo de dato
+'''
+print(type("Hello"))
+print(type(2))
+print(type(2.5))
+print(True)
+
---
+++ 01_variables.py
@@ -0,0 +1,23 @@
+# Variables
+new_str_variable = "New String Variable"
+print(new_str_variable)
+
+new_int_variable = 33
+print(new_int_variable)
+
+# Concatenate
+print(new_str_variable, ':', new_int_variable)
+
+# Some System Functions
+print(len(new_str_variable))
+
+# Variables in the same line
+name, surname, alias, age = "Juan David", "Corrales", "Juanda", 24
+print("Me llamo:", name, surname, ",pero me llaman", alias, ". Mi edad es:", age)
+
+# Inputs
+first_name = input("¿What is your name?")
+age = input("¿How old are you?")
+
+print(name)
+print(age) 

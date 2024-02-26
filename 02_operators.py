bbe27d3b910e6162bec92f96608dd21f3ff34b2e
juandaaa1 <jcg700@alumnos.unican.es>
2024-02-26 17:47:22 +0100

    Add files day 2 training

--- 02_operators.py
+++ 02_operators.py
@@ -1,1 +1,43 @@
-### Operators ###
+### Arithmetic Operators ### 
+
+print("Arithmetic Operators")
+print (3 + 4)
+print (3 - 4)
+print (3 / 4)
+print (3 * 4)
+print (16 % 4)
+print (18 % 5)
+
+print (18 // 5)
+print (2 ** 4)
+print (2 * 4 + 10 - 4 ** 2 % 12)
+
+print ("Hello " + "World")
+print ("Hello " * 4)
+print()
+
+### Comparative Operators ### 
+print("Comparative Operators")
+print (3 > 4)
+print (3 < 4)
+print (3 >= 4)
+print (3 <= 4)
+print (3 == 4)
+print (3 != 4)
+print (3 > 4 == 2)
+print()
+
+
+print("Hello" > "Python")
+print("Hello" < "Python")
+print("aaaa" >= "abaa") # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa")) # Cuenta caractereres
+print("Hello" <= "Python")
+print("Hello" == "Python")
+print("Hello" != "Python")
+
+
+### Logics Operators ### 
+print (3 > 4 and "Hello" > "Python")
+print (3 > 4 or "Hello" > "Python")
+print (not(3 > 4))

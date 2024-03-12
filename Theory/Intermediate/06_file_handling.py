6125534b8dd4a9beebf8714ab66080b49a12e3eb
juandaaa1 <jcg700@alumnos.unican.es>
2024-03-12 23:29:49 +0100

    Add more files day 12 training

--- Theory/Intermediate/06_file_handling.py
+++ Theory/Intermediate/06_file_handling.py
@@ -1,2 +1,22 @@
 ### FILE HANDLING ###
 
+import os
+
+# .txt file
+txt_file = open("Theory/Intermediate/my_file.txt", "w+")    # Leer y escribir
+txt_file.write("Mi nombre es Luis\nMi apellido es Gonzalez\n40 años\nPython\n.NET")
+
+# print(txt_file.read())    # Lee todo el txt
+# print(txt_file.read(10))  # Lee loos primeros 10 caracteres
+# print(txt_file.readline())
+# print(txt_file.readline())
+# print(txt_file.readlines())
+for line in txt_file.readlines():
+    print(line)
+    
+txt_file.write("\nTambien me gusta el lenguaje Java")
+print(txt_file.readline())
+
+txt_file.close()
+
+# os.remove("Theory/Intermediate/my_file.txt")

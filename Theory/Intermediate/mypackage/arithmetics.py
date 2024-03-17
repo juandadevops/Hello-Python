3408f3ab7fffc33c23331e787b17638f2f7edb9d
juandaaa1 <jcg700@alumnos.unican.es>
2024-03-17 11:01:36 +0100

    Reformat all the files in the intermediate folder

---
+++ Theory/Intermediate/mypackage/arithmetics.py
@@ -0,0 +1,30 @@
+### Arithmetics ###
+
+# mypackage/arithmetics.py
+# arithmetics.py
+
+def add_numbers(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+
+def subtract(a, b):
+    return (a - b)
+
+
+def multiple(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
+
+
+def remainder(a, b):
+    return a % b
+
+
+def power(a, b):
+    return a ** b
---
+++ Theory/Intermediate/mypackage/greet.py
@@ -0,0 +1,7 @@
+### GREET ###
+
+# mypackage/greet.py
+# greet.py
+
+def greet_person(firstname, lastname):
+    return f'{firstname} {lastname}, welcome to this package!'
